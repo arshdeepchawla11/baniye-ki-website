fb5c00feab842a9de2043bd3e6a7ce4d7521fe72
arshdeepchawla11 <arshdeepchawla11@gmail.com>
2026-02-08 06:23:53 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+$(".btn1").click(function () {
+    // YES clicked
+     window.location.href = "./pages/yes.html";
+});
+
+$(".btn2").click(function () {
+    // NO clicked → go to no.html
+    window.location.href = "./pages/no.html";
+});
