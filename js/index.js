b119d2d9aad441d1c727e7d15b3aa03725c57705
arshdeepchawla11 <arshdeepchawla11@gmail.com>
2026-02-08 07:23:36 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
+$(".btn1").click(function () {
+    // YES clicked
+     window.location.href = "./pages/yes.html";
+});
+
+$(".btn2").click(function () {
+    // NO clicked → go to no.html
+    window.location.href = "./pages/no.html";
+});
 
